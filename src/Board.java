f328764f4065d266ff8ecc6e7a99e6e223081642
Geir Kihle Hanssen <geir.kihle.hanssen@gmail.com>
2013-11-21 19:48:32 +0100

    Esthetic changes

--- src/Board.java
+++ src/Board.java
@@ -174,6 +174,7 @@ public class Board {
             System.out.println("|");
             System.out.println("------------------------------------");
         }
+        System.out.println("------------------------------------");
         System.out.println("|  1 |  2 |  3 |  4 |  5 |  6 |  7 |");
     }
 }
